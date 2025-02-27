ea7054066ea6149fe2af93967d1952146b0b0419
方旭佳 <3478764404@qq.com>
2025-02-27 21:04:57 +0800

    面包屑

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -12,7 +12,11 @@ const categoryStore = useCategoryStore()
       <h1 class="logo">
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
+     
       <ul class="app-header-nav">
+        <li class="home">
+          <RouterLink to="/">首页</RouterLink>
+        </li>
         <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
           <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         </li>
